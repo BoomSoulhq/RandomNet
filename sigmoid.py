5126b81783796782b16ab1ef3bf159146826a8ca
BoomSoulhq <52875340+BoomSoulhq@users.noreply.github.com>
2024-02-27 20:52:52 +0800

    Update sigmoid.py

--- sigmoid.py
+++ sigmoid.py
@@ -5,13 +5,15 @@ import torch
 from torch.optim import Adam
 from torch import optim
 import math
-
 import os
-#清清是傻瓜
+
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
-# <editor-fold desc ="拉格朗日差值"
-"""拉格朗日插值公式"""
+global em_x, em_y, cut_ratio
+
+"""
+拉格朗日插值公式
+"""
 
 
 def L(xinput: int, k: int, xcord: list):  # 拉格朗日基函数
@@ -25,15 +27,12 @@ def lagrange(xinput: int, xcord: list, ycord: list):
     return res
 
 
-# </editor-fold>
+"""
+第一次拟合sigmoid拟合函数(体外通路活性->体外浓度)
+"""
 
 
-# <editor-fold desc ="sigmoid拟合"
-"""sigmoid拟合函数"""
-'''第一版'''
-
-
-def sigmoid(x, L, x0, k, b):
+def sigmoid(x, L, x0, k, b):  # sigmoid基函数
     y = L / (1 + np.exp(-k * (x - x0))) + b
     return y
 
@@ -49,14 +48,34 @@ def sigmoid_fit(x_data, y_data, x_range):
     return sigmoid(x_range, *popt)
 
 
-'''第二版'''
+"""
+线性拟合函数(体外浓度->体内浓度)
+"""
+
+
+def linear_function(x, a, b):  # 线性函数基函数
+    return a * x + b
+
+
+def linear_fit(x_data, y_data, x_range):
+    params, _ = curve_fit(linear_function, x_data, y_data)
+    print("线性拟合参数:", params)
+    # 提取拟合参数值
+    slope, intercept = params
+
+    return slope * x_range + intercept
+
+
+"""
+第二次拟合Sigmoid函数(体内浓度->体内通路活性)
+"""
 
 
 def eq(x):
     return (x * math.log(x / (cut_ratio * cut_ratio))) - 4 * (em_y)
 
 
-def Sigmoid(x_range, x_data, d):
+def Sigmoid(x_range, x_data, d):  # Sigmoid基函数
     x_guess = 2.0
     L = 4 * (fsolve(eq, x_guess)[0])
     b = (x_data[-1] * math.log(L / cut_ratio) - x_data[0] * math.log(cut_ratio)) / em_x
@@ -99,41 +118,16 @@ def Sigmoid_fit(x_data, y_data, x_range):
     # ax.plot(xrange.numpy(), y_predict.detach().numpy(), label="predict")
     # ax.plot(xdata.numpy(), ydata.numpy(), label="true")
     # ax.legend()
-    #plt.show(block=True)
+    # plt.show(block=True)
     return y_predict.detach().numpy()
 
 
-# </editor-fold>
-
-# <editor-fold desc ="线性函数拟合"
-"""线性拟合函数"""
-
-
-# 定义线性函数
-def linear_function(x, a, b):
-    return a * x + b
-
-
-# 使用curve_fit进行拟合
-def linear_fit(x_data, y_data, x_range):
-    params, _ = curve_fit(linear_function, x_data, y_data)
-    print("线性拟合参数:", params)
-    # 提取拟合参数值
-    slope, intercept = params
-
-    return slope * x_range + intercept
-
-
-# </editor-fold>
-
-# <editor-fold desc ="三次指数平滑数据预测"
-"""时间序列预测"""
+"""三次指数平滑时间序列预测处理数据不等长问题"""
 
 
 def time_seq(seqInput: list, length: int):
     global a, b, c
     alpha = [0.1 * i for i in range(1, 10)]  # 加权系数选择
-    # alpha = [0.3]
     if len(seqInput) <= 3:
         return "Warning：Insufficient Data!"
     init = (seqInput[0] + seqInput[1] + seqInput[2]) / 3
@@ -156,7 +150,6 @@ def time_seq(seqInput: list, length: int):
             c = (s1 - 2 * s2 + s3) * np.power(w, 2) / (2 * np.power(1 - w, 2))
             # print("第{}次预测".format(i + 1))
             # print("一次平滑值:{},二次平滑值:{},三次平滑值:{}".format(s1, s2, s3))
-            # print(" ")
             res[i] = a + b + c
         for i in range(1, len(seqInput)):
             cur_error += np.power((res[i - 1] - seqInput[i]), 2)
@@ -167,8 +160,6 @@ def time_seq(seqInput: list, length: int):
             A, B, C = a, b, c
             S1, S2, S3 = s1, s2, s3
             best_w = w
-        # print("cur_error:", cur_error)
-        # print("=" * 100)
     last_id = seqInput[-1]
     S1 = best_w * last_id + (1 - best_w) * S1
     S2 = best_w * S1 + (1 - best_w) * S2
@@ -186,11 +177,9 @@ def time_seq(seqInput: list, length: int):
     return output
 
 
-# print(time_seq([20.04, 20.06, 25.72, 34.61, 51.77, 55.92, 80.65, 131.11, 148.58, 162.67, 232.26], 2))
-# </editor-fold>
-
-# <editor-fold desc ="输入数据排序"
-"""排序"""
+"""
+排序
+"""
 
 
 def bubble_sort(arr1, arr2):
@@ -207,119 +196,107 @@ def bubble_sort(arr1, arr2):
     return arr1, arr2
 
 
-# </editor-fold>
+def data_test(m, n, x, y, show_ratio: int):
+    global em_x, em_y, cut_ratio
+    x, y = bubble_sort(x, y)
+    # test_mol_n = np.linspace(np.min(n), np.max(n) + 20, 1000)  # 测试的体外通路活性
+    # test_mol_n = np.linspace(np.min(n), np.max(n), 1000)
+    test_mol_n = np.linspace(np.min(n), np.max(n), 1000)  # 测试的体外通路活性
 
-# <editor-fold desc ="已知数据"
-"""这里输入数据"""
-m = np.array([0, 1.256, 6.28, 31.4])  # 体外浓度
-n = np.array([21.178, 20.054, 19.635, 18.589])  # 体外通路活性
-m, n = bubble_sort(m, n)
+    """拟合n->m"""
+    # test_mol_m = np.array([lagrange(n0, list(n), list(m)) for n0 in test_mol_n])  # 映射n—>m
+    test_mol_m = np.array(sigmoid_fit(n, m, test_mol_n))  # sigmoid拟合映射n—>m
 
-# x = np.array([0, 5,8, 10])
-x = np.array([0, 30, 100, 300])  # 体内浓度
-y = np.array([28.18,27.69, 33.39, 36.98])  # 体内通路活性
-x, y = bubble_sort(x, y)
-# </editor-fold>
+    """时间序列预测补全数据"""
+    if len(m) != len(x):
+        if len(m) < len(x):
+            n = time_seq(list(n), len(x) - len(n))
+            m = time_seq(list(m), len(x) - len(m))
+        else:
+            x = time_seq(list(x), len(m) - len(x))
+            y = time_seq(list(y), len(m) - len(y))
 
-# <editor-fold desc ="测试数据"
-#test_mol_n = np.linspace(np.min(n), np.max(n) + 20, 1000)  # 测试的体外通路活性
-# test_mol_n = np.linspace(np.min(n), np.max(n), 1000)
-test_mol_n = np.linspace(15, np.max(n), 1000)  # 测试的体外通路活性
+    """拟合m->x"""
+    test_mol_x = np.array(linear_fit(m, x, test_mol_m))
+    max_x = max(x)
+    min_x = min(y)
+    max_test_x = max(test_mol_x)
+    min_test_x = min(test_mol_x)
+    test_mol_x = np.array(
+        [(min_x + (x - min_test_x) * (max_x - min_x) / (max_test_x - min_test_x)) for x in
+         test_mol_x])  # 统一x与test_mol_x的尺度
 
-"""拟合n->m"""
-# test_mol_m = np.array([lagrange(n0, list(n), list(m)) for n0 in test_mol_n])  # 映射n—>m
-test_mol_m = np.array(sigmoid_fit(n, m, test_mol_n))  # sigmoid拟合映射n—>m
+    em_x = x[-1] - x[0]
+    em_y = y[-1] - y[0]
 
-"""时间序列预测补全数据"""
-if len(m) != len(x):
-    if len(m) < len(x):
-        n = time_seq(list(n), len(x) - len(n))
-        m = time_seq(list(m), len(x) - len(m))
-    else:
-        x = time_seq(list(x), len(m) - len(x))
-        y = time_seq(list(y), len(m) - len(y))
+    """拟合x->y"""
+    test_mol_y = np.array(Sigmoid_fit(x, y, list(test_mol_x)))
 
-global em_x, em_y, cut_ratio
-cut_ratio = 0.01
-em_x = x[-1] - x[0]
-em_y = y[-1] - y[0]
+    plt.rcParams['font.sans-serif'] = [u'simHei']
+    plt.rcParams['axes.unicode_minus'] = False
 
-"""拟合m->x"""
-# test_mol_x = np.array([lagrange(m0, list(m), list(x)) for m0 in test_mol_m])  # 映射m—>x
-test_mol_x = np.array(linear_fit(m, x, test_mol_m))
+    plt.subplot(2, 2, 1)
+    plt.title("P1")
+    plt.xlabel("体外通路活性")
+    plt.ylabel("体外浓度")
+    # plt.scatter(test_mol_n, test_mol_m, color='violet')
+    plt.scatter(n, m, color='violet')
 
-max_val=max(test_mol_x)
-min_val=min(test_mol_x)
-test_mol_x=np.array([max_val*(x-min_val)/(max_val-min_val) for x in test_mol_x])
-print("test_mol_x:", list(test_mol_x))
+    plt.subplot(2, 2, 2)
+    plt.title("P2")
+    plt.xlabel("体外浓度")
+    plt.ylabel("体内浓度")
+    plt.scatter(m, x, color='violet')
 
-"""拟合x->y"""
-# test_mol_y = np.array([lagrange(x0, list(x), list(y)) for x0 in test_mol_x])  # 映射x—>y
-# test_mol_y = np.array(sigmoid_fit(x,y,test_mol_x))  #sigmoid拟合映射x—>y
-test_mol_y = np.array(Sigmoid_fit(x, y, list(test_mol_x)))  # sigmoid拟合映射x—>y
+    plt.subplot(2, 2, 3)
+    plt.title("P3")
+    plt.xlabel("体内浓度")
+    plt.ylabel("体内通路活性")
+    plt.scatter(x, y, color='violet')
 
-# </editor-fold>
-
-
-plt.rcParams['font.sans-serif'] = [u'simHei']
-plt.rcParams['axes.unicode_minus'] = False
-
-plt.subplot(2, 2, 1)
-plt.title("P1")
-plt.xlabel("体外通路活性")
-plt.ylabel("体外浓度")
-# plt.scatter(test_mol_n, test_mol_m, color='violet')
-plt.scatter(n, m, color='violet')
-
-plt.subplot(2, 2, 2)
-plt.title("P2")
-plt.xlabel("体外浓度")
-plt.ylabel("体内浓度")
-# plt.scatter(test_mol_n, test_mol_m, color='violet')
-plt.scatter(m, x, color='violet')
-
-plt.subplot(2, 2, 3)
-plt.title("P3")
-plt.xlabel("体内浓度")
-plt.ylabel("体内通路活性")
-# plt.scatter(test_mol_n, test_mol_x, color='violet')
-plt.scatter(x, y, color='violet')
-
-plt.subplot(2, 2, 4)
-plt.title("拟合")
-plt.xlabel("体外通路活性")
-plt.ylabel("体内通路活性")
-print(test_mol_x)
-# print(test_mol_y)
-plt.scatter(test_mol_n, test_mol_y, color='violet',s = 5)
-#plt.scatter(test_mol_n, np.array([min(test_mol_y)*1.2 for _ in range(len(test_mol_n))]),color = "black")
+    plt.subplot(2, 2, 4)
+    plt.title("拟合")
+    plt.xlabel("体外通路活性")
+    plt.ylabel("体内通路活性")
+    plt.scatter(test_mol_n, test_mol_y, color='violet', s=5)
+    ratio(show_ratio=show_ratio, test_mol_n=test_mol_n, test_mol_y=test_mol_y)
+    plt.show()
 
 
 """
 show_ratio:设置需要查看纵坐标的比例,取值在0-100
 """
-def ratio(show_ratio:int):
-    if show_ratio<0 or show_ratio>100:
-        return "Invalid value!"
-    for i in range(len(test_mol_n)-1):
-        sign = min(test_mol_y)+ (max(test_mol_y)-min(test_mol_y)) * show_ratio * 0.01
-        if test_mol_y[i]<= sign <=test_mol_y[i+1]:
-            plt.plot(test_mol_n,
-                        np.array([sign for _ in range(len(test_mol_y))]), "--",color = "black" ,)
-            plt.plot(np.array([test_mol_n[i] for _ in range(len(test_mol_y))]),test_mol_y, "--",color = "black" ,)
-            plt.text(test_mol_n[i],sign + 5 , "纵坐标{}%".format(show_ratio),fontdict={'size':12,'color':"red"})
-            print("纵坐标为{}%时对应的坐标为:".format(show_ratio),(test_mol_n[i], sign))
-            break
-ratio(50)
-# plt.scatter(n, y, color='violet')
-# plt.grid(True)
-plt.show()
 
-# num_states = 5
-# mutation_num = 25
-# m_prob = 0.1
-# res = np.random.choice(np.arange(0,2), (mutation_num, num_states+1), p=[1-m_prob, m_prob])
-#
-# keep_top_k = [[1,3,6,5,13,5,6],[1,2,3,4,1],[1,4,5,1,5,6,8]]
-# keep_top_k = sorted(keep_top_k, key=lambda can:can[-1])
-# print(keep_top_k)
+
+def ratio(show_ratio: int, test_mol_n, test_mol_y):
+    if show_ratio < 0 or show_ratio > 100:
+        return "Invalid show_ratio value!"
+    flag = False
+    for i in range(len(test_mol_n) - 1):
+
+        sign = min(test_mol_y) + (max(test_mol_y) - min(test_mol_y)) * show_ratio * 0.01
+        if test_mol_y[i] <= sign <= test_mol_y[i + 1]:
+            plt.plot(test_mol_n,
+                     np.array([sign for _ in range(len(test_mol_y))]), "--", color="black", )
+            plt.plot(np.array([test_mol_n[i] for _ in range(len(test_mol_y))]), test_mol_y, "--", color="black", )
+            plt.text(test_mol_n[i], sign, "纵坐标{}%".format(show_ratio), fontdict={'size': 12, 'color': "red"})
+            flag = True
+            print("纵坐标为{}%时对应的坐标为:".format(show_ratio), (test_mol_n[i], sign))
+            break
+    if not flag:
+        return "Invalid Error"
+
+
+if __name__ == "__main__":
+    #想看清清穿黑丝
+    """这里输入数据"""
+    m = np.array([0, 1.256, 6.28, 31.4])  # 体外浓度
+    n = np.array([21.178, 50, 19.635, 18.589])  # 体外通路活性
+    m, n = bubble_sort(m, n)
+
+    # x = np.array([0, 5,8, 10])
+    x = np.array([50, 30, 100, 500])  # 体内浓度
+    y = np.array([28.18, 27.69, 33.39, 36.98])  # 体内通路活性
+
+    cut_ratio = 0.00001  # 这个参数越小最后输出的曲线保留的部分越多设置在0.01以下均可
+    data_test(m, n, x, y, show_ratio=80)  # show_ratio 是指显示的比例
