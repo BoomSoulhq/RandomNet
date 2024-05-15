c8cd476be077d2ff9820a4d25e5e9c77fef3cb84
BoomSoulhq <52875340+BoomSoulhq@users.noreply.github.com>
2024-05-15 20:41:09 +0800

    Update 科学上网.py

--- 科学上网.py
+++ 科学上网.py
@@ -2,20 +2,22 @@ import pandas as pd  # 一定记得改通路条数！！
 import os
 import re
 
-data = pd.read_csv(r"D:\Desktop\赵-NB通路\表达量\15-ZL-000007-Ca1.csv")
-os.mkdir(r"D:\Desktop\赵-NB通路\通路\15-ZL-000007-Ca1-7条通路.csv")
-header = pd.read_excel(r"D:\Desktop\s2.xlsx").keys().tolist()
+from numpy.core.defchararray import isdigit
+
+data = pd.read_csv("15-ZL-000007-Ca1.csv")
+os.mkdir(r"D:\users\p30057372\some_script\2024-5-15\15-ZL-000007-Ca1-7条通路")
+header = pd.read_excel(r"D:\users\p30057372\some_script\2024-5-15\s2.xlsx").keys().tolist()
 
 rstr = r"[\/\\\:\*\?\"\<\>\|]"
 for i in range(len(header)):
     header[i] = re.sub(rstr, " ", header[i])
 
 for j in range(7):
-    file = r"D:\Desktop\赵-NB通路\通路\15-ZL-000007-Ca1-7条通路\{name}.csv".format(name=header[j])
-    source = pd.read_excel(r"D:\Desktop\s2.xlsx").iloc[:, j].values.tolist()
+    file = r"D:\users\p30057372\some_script\2024-5-15\15-ZL-000007-Ca1-7条通路\{name}.csv".format(name=header[j])
+    source = pd.read_excel(r"D:\users\p30057372\some_script\2024-5-15\s2.xlsx").iloc[:, j].values.tolist()
     source = [x for x in source if not pd.isnull(x)]
     for i in source:
-        result = data[data.iloc[:, 0].str.lower() == i.lower()]
+        result = data[data.iloc[:, 0].apply(lambda x: x.lower() if not isdigit(x) else "unknow") == i.lower()]
         if result.empty:
             print("查不到基因{name}的相关数据".format(name=i))
         if not os.path.exists(file):
